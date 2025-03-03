87524e513fe662048713869bf3555f474e9afcb5
Salva297 <salgonger1997@gmail.com>
2025-03-03 19:10:37 +0100

    Added new features to load env variables

--- shacl_aggregate_app/__init__.py
+++ shacl_aggregate_app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 import sys
 import secrets
+from dotenv import load_dotenv
+import os
 sys.stdout.flush()
 
+
+# Load .env
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = '3ed554768ed1ccf065f80979d1840f2d' #secrets.token_hex(16)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key') #secrets.token_hex(16)
 
     from .blueprints.render import render as render_blueprint
     app.register_blueprint(render_blueprint)
