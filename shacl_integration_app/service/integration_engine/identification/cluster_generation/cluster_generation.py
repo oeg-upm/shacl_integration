4e0bec933e62556b8906b07902daf089ac681f8c
Salva5297 <salgonger1997@gmail.com>
2025-03-22 21:16:10 +0100

    Added final functionalities to transitive alignment now working

--- shacl_integration_app/service/integration_engine/identification/cluster_generation/cluster_generation.py
+++ shacl_integration_app/service/integration_engine/identification/cluster_generation/cluster_generation.py
@@ -22,9 +22,9 @@ class ClusterGeneration:
         The function `execute_alignment` parses an alignment reference, queries the results based on a
         threshold, and stores the alignment tuples in a list.
         """
-        print(self.alignment_reference)
         if self.alignment_reference is None:
             self.ontology_alignment_results = None
+            # Generate code in order to align ontologies
             pass
         else:
             threshold: str = str(os.getenv('REFERENCE_THRESHOLD', 1.0))
@@ -35,8 +35,48 @@ class ClusterGeneration:
         
     @get_time        
     def execute_transitive_alignment(self) -> list[tuple[str]]:
+        path_list: list[str] = ['http://www.w3.org/ns/shacl#targetClass', 'http://www.w3.org/ns/shacl#path', 'http://www.w3.org/ns/shacl#targetObjectsOf', 'http://www.w3.org/ns/shacl#targetSubjectsOf']
+        hlt_tuples: list[tuple[str]] = [tup for tup in self.tuple_result_list if tup[2] not in path_list]
+        new_hlt_tuples = []
+
+        for hlt in hlt_tuples:
+            new_hlt = list(hlt)  # Convert the tuple into a list to allow modification of its elements
+            flag: int = 0
+            for alignment in self.alignment_tuples_result:
+                if new_hlt[0] == alignment[0] or new_hlt[0] == alignment[1]:
+                    flag += 1
+                    new_hlt[0] = (alignment[0], alignment[1])
+                if new_hlt[1] == alignment[0] or new_hlt[1] == alignment[1]:
+                    flag += 1
+                    new_hlt[1] = (alignment[0], alignment[1])
+                if new_hlt[2] == alignment[0] or new_hlt[2] == alignment[1]:
+                    flag += 1
+                    new_hlt[2] = (alignment[0], alignment[1])
+            if flag == 2:
+                new_hlt_tuples.append(tuple(new_hlt))
+
+        for i in range(len(new_hlt_tuples)):
+            for j in range(i + 1, len(new_hlt_tuples)):
+                t1 = set(new_hlt_tuples[i])
+                t2 = set(new_hlt_tuples[j])
+                common: set = t1.intersection(t2)
+                if len(common) == 2:
+                    diff_t1: str | tuple = list(t1 - common)[0]
+                    diff_t2: str | tuple = list(t2 - common)[0]
+                    if type(diff_t1) is not tuple and type(diff_t2) is not tuple:
+                        namespace_1 = self.get_namespace(diff_t1)
+                        namespace_2 = self.get_namespace(diff_t2)
+                        if namespace_1 != namespace_2:
+                            self.alignment_tuples_result.append((diff_t1, diff_t2))
         return self.alignment_tuples_result
 
+
+    def get_namespace(self, uri: str) -> str:
+        """Extracts the prefix or namespace from a URI."""
+        if '#' in uri:
+            return uri.split('#')[0]
+        else:
+            return '/'.join(uri.split('/')[:-1])
 
     @get_time
     def execute_cluster_generation(self) -> list[Cluster]:
